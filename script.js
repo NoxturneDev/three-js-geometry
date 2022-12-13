eb859e25e76b56fc32fb995867704bee7867064d
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-12-13 19:10:55 +0700

    git init

---
+++ script.js
@@ -0,0 +1,36 @@
+import animate from './lib/animation.js'
+
+animate()
+// /** @type {HTMLCanvasElement} */
+// const canvas = doc.getElementById('canvas')
+// const ctx = canvas.getContext('2d')
+
+
+// let i = 0
+// function animate() {
+//     i++
+
+//     const cx = canvas.width / 2
+//     const cy = canvas.height / 2
+
+//     ctx.strokeStyle = "red"
+//     ctx.lineWidth = 2
+
+//     const bx = cx + i
+//     const by = cy + i
+
+//     ctx.beginPath()
+//     ctx.moveTo(cx, cy)
+//     ctx.lineTo(cx + i, cy + 100 - i)
+//     ctx.stroke()
+
+//     const animation = window.requestAnimationFrame(animate)
+//     console.log('frames ' + animation)
+// }
+
+// function init() {
+//     animate()
+// }
+
+// init()
+
